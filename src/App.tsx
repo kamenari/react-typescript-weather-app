44a3155ed1931e33853cda4de8435e3a4cda612e
takahashikazunari <kamenaris@gmail.com>
2021-07-11 14:40:55 +0900

    Formコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
 import './App.css';
 
 function App() {
   return (
     <div className="test">
       <Title />
+      <Form />
+      <Results />
     </div>
   );
 }
