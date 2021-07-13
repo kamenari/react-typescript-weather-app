0cb95cf2f39db174593e8eb7633306c22e934bb1
takahashikazunari <kamenaris@gmail.com>
2021-07-13 12:59:04 +0900

    Resultsコンポーネント作成、ロジックをappに移す、スタイル適用、APIにcity名を渡すためにリテラルテンプレートに修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import React, { useState } from "react";
+
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
 import './App.css';
 
+type ResultsStateType = {
+  country : string
+  cityName : string
+  temperture : string
+  conditionText : string
+  icon : string
+}
+
 function App() {
+  const [city, setCity] = useState<String>();
+  const [results, setResults] = useState<ResultsStateType>({
+    country : " ",
+    cityName : " ",
+    temperture : " ",
+    conditionText : " ",
+    icon : " ",
+  })
+  const getWeather = (e : React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      fetch(`https://api.weatherapi.com/v1/current.json?key=646bbaa5147b4550b6a54047211007&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperture: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon
+        })
+      })
+  }
   return (
-    <div className="test">
-      <Title />
-      <Form />
-      <Results />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
 
-const Form = () => {
-    const [city, setCity] = useState<String>();
-    const getWeather = (e : any) => {
-        e.preventDefault();
-        fetch("https://api.weatherapi.com/v1/current.json?key=646bbaa5147b4550b6a54047211007&q=London&aqi=no")
-        .then(res => res.json())
-        .then(data => console.log(data)
-        )
-    }
+type FormPropsType = {
+    setCity : React.Dispatch<React.SetStateAction<String | undefined>>
+    getWeather :(e: React.FormEvent<HTMLFormElement>) => void
+}
+
+const Form = (props : FormPropsType) => {
     return (
-        <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e  => setCity(e.target.value)} />
-            <button type="submit" onClick={getWeather}> Get Weather</button>
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e  => props.setCity(e.target.value)} />
+            <button type="submit"> Get Weather</button>
         </form>
     );
 };
