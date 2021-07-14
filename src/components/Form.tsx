7817aeb303d02ce948d5cf0d61c322627ef9de2e
takahashikazunari <kamenaris@gmail.com>
2021-07-14 13:54:59 +0900

    getWeatherを実行時にinputのテキストを削除する、cityの型はStringだとエラーになるのでanyで代用

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,14 @@
 
 type FormPropsType = {
-    setCity : React.Dispatch<React.SetStateAction<String | undefined>>
-    getWeather :(e: React.FormEvent<HTMLFormElement>) => void
+    city: any;
+    setCity : React.Dispatch<React.SetStateAction<String | undefined>>;
+    getWeather :(e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const Form = ({ setCity, getWeather } : FormPropsType) => {
+const Form = ({ city,  setCity, getWeather } : FormPropsType) => {
     return (
         <form onSubmit={getWeather}>
-            <input type="text" name="city" placeholder="都市名" onChange={e  => setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="都市名" onChange={e  => setCity(e.target.value)} value={city} />
             <button type="submit"> Get Weather</button>
         </form>
     );
